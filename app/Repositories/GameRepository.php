fd8500264e35ae79f1f3553daf10102be3410445
Kieron Yorke <kieron.yorke@fidomoney.com>
2022-06-06 22:06:48 +0100

    refactor game repository to make it testable

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -2,14 +2,21 @@
 
 namespace App\Repositories;
 
-use app\Dto\GameDto;
+use App\Dto\GameDto;
 use App\Models\Game;
+use Illuminate\Database\Eloquent\Model;
 
 class GameRepository
 {
+
+    public function __construct(Model $model)
+    {
+       $this->model = get_class($model);
+    }
+
     public function getAll(): array
     {
-        $games = Game::all();
+        $games = ($this->model)::all();
         $returnGames = [];
 
         foreach ($games as $game) {
@@ -26,7 +33,7 @@ class GameRepository
 
     public function find(int $id): GameDto
     {
-        $game = Game::findOrFail($id);
+        $game = ($this->model)::findOrFail($id);
 
         return new GameDto(
             $game->id,
@@ -38,11 +45,11 @@ class GameRepository
 
     public function create(GameDto $game) 
     {
-        $game = new Game([
-            'name' => $game->getName(),
-            'price' => $game->getPrice(),
-            'index' => $game->getIndex(),
-        ]);
+        $game = new $this->model;
+
+        $game->name = $game->getName();
+        $game->price = $game->getPrice();
+        $game->index = $game->getIndex();
 
         $game->save();
     }
