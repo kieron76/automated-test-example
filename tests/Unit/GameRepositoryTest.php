da64c4f7d9e4fda0fae9f218b12dc9946125fb5f
Kieron Yorke <kieron.yorke@fidomoney.com>
2022-06-07 08:39:25 +0100

    game repository test

---
+++ tests/Unit/GameRepositoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Dto\GameDto;
+use App\Models\Game;
+use App\Repositories\GameRepository;
+
+it('can get all records', function() {
+
+    $game = new StdClass();
+    $game->id = 1;
+    $game->name = 'Monopoly';
+    $game->price = '50';
+    $game->index = 3;
+
+    $mock = Mockery::mock(Game::class);
+    $mock->shouldReceive('all')
+        ->once()
+        ->andReturn([
+            $game
+        ]);
+
+    
+    $gameRepo = new GameRepository($mock);
+    $games = $gameRepo->getAll();
+
+    expect($games)->toBeArray();
+});
+
+it('returns null when no records', function() {
+
+    $mock = Mockery::mock(Game::class);
+    $mock->shouldReceive('all')
+        ->once()
+        ->andReturn(null);
+
+    $gameRepo = new GameRepository($mock);
+    $games = $gameRepo->getAll();
+
+    expect($games)->toBeNull();
+});
